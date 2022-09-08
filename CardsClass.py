ae1afc6dd95031527399bf65e627683c66d4c0ed
Julien Campbell <66806548+julien-campbell@users.noreply.github.com>
2022-09-08 22:55:16 +0000

    Add class card via CardsClass.py, add back import text to main

---
+++ CardsClass.py
@@ -0,0 +1,42 @@
+class card:
+  suits = ["diamonds","clubs", "spades",  "hearts"]
+  ranks = ["narf", "narf", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"]
+
+
+  def __init__(self, suit, rank):
+    self.suit = suit
+    self.rank = rank
+
+
+    
+    
+  def __str__(self):
+    return (self.ranks[self.rank] + " of " + self.suits[self.suit])
+  
+  def cmp(self, other):
+  # Check the suits
+    if self.suit > other.suit: return 1
+    if self.suit < other.suit: return -1
+    # Suits are the same... check rank
+    if self.rank > other.rank: return 1
+    if self.rank < other.rank: return -1
+    # Ranks are the same... it's a tie
+    return 0  
+
+  def __eq__(self, other):
+    return self.cmp(other) == 0
+
+  def __le__(self, other):
+      return self.cmp(other) <= 0
+
+  def __ge__(self, other):
+      return self.cmp(other) >= 0
+
+  def __gt__(self, other):
+      return self.cmp(other) > 0
+
+  def __lt__(self, other):
+      return self.cmp(other) < 0
+
+  def __ne__(self, other):
+      return self.cmp(other) != 0
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from Hearts import Hearts
+from OldMaid import oldmaid
+from UsernamesandPlayers import players
 
+# #Playing OldMaid
+# game = oldmaid()
+# Users = players()
+# game.setup(Users)
+# game.play_game()
+
+
+#Playing Hearts
+#Users = players()
+#game = Hearts(Users)
+#game.setup_hearts()
+#game.play_hearts()
